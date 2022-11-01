19381db177f0664df017ca98abdf49dfeae641a6
Alkan <simsekalkan11@gmail.com>
2022-11-01 15:01:00 +0300

    Models yapısı oluşturuldu

--- base/urls.py
+++ base/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path,include
+from .views import taskList
+
+urlpatterns = [
+    path('',taskList.as_view(),name='home'),
+]
 
--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-# Create your views here.
+
+class taskList(ListView):
+    model=Task
+    context_object_name='tasks'
